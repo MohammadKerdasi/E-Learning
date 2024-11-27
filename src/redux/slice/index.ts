be21cf5733b98dd759e29637fbb6ae9b5cb9d9b2
lourina-karkoa <nanasy893@gmail.com>
2024-11-27 11:53:17 +0300

    reducer

---
+++ src/redux/slice/index.ts
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={
+    
+}
+
+
+export const courseSlice = createSlice({
+    name: "courses",
+    initialState,
+    reducers: {
+      
+    },
+  });
+
+
+export default courseSlice.reducer;
