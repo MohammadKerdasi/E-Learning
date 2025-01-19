a0bc2ff776b1b2076eaf836a2390098815322df5
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2025-01-19 20:32:32 +0300

    updates

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -13,20 +13,21 @@ export default function SideBar({ isMenuOpen, setIsMenuOpen, navData }: SideBarP
       >
       </div>
            <div
-        className={`fixed top-0 right-0 h-screen w-full max-w-sm bg-White/95 z-50 transform transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-screen  w-full max-w-sm bg-White/95 z-50 transform transition-transform duration-300 ${
           isMenuOpen ? "translate-x-0 shadow-lg" : "translate-x-full"
         }`}
       >
-        <div className="flex flex-col h-full">
-          <ul className="lg:hidden flex flex-col items-center justify-center gap-5 flex-grow">
+        <div className="flex flex-col min-h-screen  ">
+          <ul className="lg:hidden flex flex-col items-center justify-center gap-y-10 flex-grow">
           {navData.map((e, i) => (
             <li key={i}>
               <NavLink
                 className={({ isActive }) =>
                   isActive
-                    ? "py-3.5 px-6 rounded-lg font-normal bg-White/95 h-full transition-all text-sm desktop:text-lg duration-300"
+                    ? "py-3.5 px-6 rounded-lg font-normal text-white bg-Orange/50 h-full transition-all text-sm desktop:text-lg duration-300"
                     : "py-3.5 px-6 rounded-lg font-normal  h-full text-sm desktop:text-lg "
                 }
+                onClick={() => setIsMenuOpen(false)}
                 to={e.path}
               >
                 {e.text}
